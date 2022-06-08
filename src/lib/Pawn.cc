516e23e986d8df2d9e95bcad00f374a773ea63f2
rmorgan10 <robertmorgan503@gmail.com>
2022-06-08 11:19:00 -0500

    This commit updates the Pawn class's moveset.
    Pawns can only move forward, and the previous implementation was correct but only for black pawns. Here the moveset is expanded to also include white pawns. Within the legal move filtering step, the pawn moves will need to be filtered based on their color.

--- src/lib/Pawn.cc
+++ src/lib/Pawn.cc
@@ -10,6 +10,6 @@ Pawn::Pawn(string current_square_val, string color_val, int base_value_val, stri
     : Piece(current_square_val, color_val, base_value_val, name_val) {}
 
 vector<vector<int>> Pawn::get_all_moves(){
-    vector<vector<int>> moves = { {1,0}, {2,0}, {1,-1}, {1,1} };
+    vector<vector<int>> moves = { {1,0}, {2,0}, {1,-1}, {1,1}, {-1,0}, {-2,0}, {-1,-1}, {-1,1} };
     return moves;
 }
